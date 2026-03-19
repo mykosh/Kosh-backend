9a3e0d4c199176dae655f6246cd89284b33e9503
mykosh <support@propredict.info>
2026-03-19 02:43:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY=https://kosh-backend-moui.onrender.com/api/kosh
 });
 
 app.post("/api/kosh", async (req, res) => {
